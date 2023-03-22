a253c3ec8ce8bf27b4a886fc6db4bc956d065bd6
MMYTCMaint <128618519+MMYTCMaint@users.noreply.github.com>
2023-03-22 09:40:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Your Firebase configuration details
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA4AHWrHdytdhRCJT48hZKuvfnhmnwOVxs",
+  authDomain: "maintenance-request-d98ba.firebaseapp.com",
+  projectId: "maintenance-request-d98ba",
+  storageBucket: "maintenance-request-d98ba.appspot.com",
+  messagingSenderId: "573805910976",
+  appId: "1:573805910976:web:1818d07cb267b91dec4625"
 };
 
 // Initialize Firebase
