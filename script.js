288081cbc946f55ddae7e484191dc16208f64fea
MMYTCMaint <128618519+MMYTCMaint@users.noreply.github.com>
2023-03-23 15:03:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,102 @@
-// Your Firebase configuration details
-const firebaseConfig = {
-  // ...
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Show/hide new request form
-document.getElementById('new-request-btn').addEventListener('click', () => {
-  const form = document.getElementById('new-request-form');
-  form.style.display = form.style.display === 'none' ? 'block' : 'none';
-});
+// Your Google Sheets API client ID
+const clientId = '164604608036-2m8htu36kk90g66dfd342fe0aavh0ugs.apps.googleusercontent.com';
+const sheetId = '1L-lDxOZR6l0b4J4O1Aqb3qyU6cL-6SdU6OeN3qztNxo'; // Replace this with the actual Sheet ID
+const sheetName = 'MaintenanceRequests';
 
 // Add new maintenance request
-document.getElementById('maintenance-request-form').addEventListener('submit', (event) => {
+document.getElementById('maintenance-request-form').addEventListener('submit', async (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const values = {};
   formData.forEach((value, key) => {
     values[key] = value;
   });
+
+  // Authenticate and save the request to Google Sheet
+  authenticate().then(loadClient).then(async () => {
+    await saveToGoogleSheet(values);
+  });
+
   addRequest(values.Location, values.Room, values.Staff, values['Date Requested'], values.Description);
 });
 
-// Toggle request status between complete and incomplete
-document.getElementById('requests-list').addEventListener('click', (event) => {
-  if (event.target.classList.contains('status-btn')) {
-    toggleRequestStatus(event.target);
+async function authenticate() {
+  return gapi.auth2.getAuthInstance().signIn({scope: "https://www.googleapis.com/auth/spreadsheets"})
+      .then(() => { console.log("Sign-in successful"); },
+            (err) => { console.error("Error signing in", err); });
+}
+
+function loadClient() {
+  gapi.client.setApiKey("AIzaSyCcYduLn5rRSVY87x6vAXqks_KugU_gi9E");
+  return gapi.client.load("https://sheets.googleapis.com/$discovery/rest?version=v4")
+      .then(() => { console.log("GAPI client loaded for API"); },
+            (err) => { console.error("Error loading GAPI client for API", err); });
+}
+
+async function saveToGoogleSheet(data) {
+  const newRowData = [
+    data.Location,
+    data.Room,
+    data.Staff,
+    data['Date Requested'],
+    data.Description,
+    'Incomplete'
+  ];
+
+  const params = {
+    spreadsheetId: sheetId,
+    range: `${sheetName}!A1:G1`,
+    valueInputOption: 'RAW',
+    insertDataOption: 'INSERT',
+  };
+
+  const requestBody = {
+    values: [newRowData],
+  };
+
+  try {
+    const response = await gapi.client.sheets.spreadsheets.values.append(params, requestBody);
+    console.log('Data saved to Google Sheet:', response.result);
+  } catch (error) {
+    console.error('Error saving data to Google Sheet:', error);
   }
-});
+}
 
-// Authenticate user on form submit
-document.getElementById('login-form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const emailInput = document.getElementById('email');
-  const email = emailInput.value;
+// Load the Google API client and auth2 library
+function initClient() {
+  gapi.load('client:auth2', {
+    callback: function() {
+      // Initialize the Google API client
+      gapi.client.init({
+        apiKey: 'AIzaSyCcYduLn5rRSVY87x6vAXqks_KugU_gi9E',
+        clientId: '164604608036-2m8htu36kk90g66dfd342fe0aavh0ugs.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/spreadsheets',
+      }).then(
+        function () {
+          // Add listener to handle the authentication status
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
 
-  if (!emailInput.checkValidity()) {
-    alert('Invalid email address. Please enter an email in the format of firstname.lastname@mmytc.com.');
-    return;
-  }
+          // Check if the user is already signed in
+          updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        },
+        function (error) {
+          console.error('Error initializing Google API client:', error);
+        }
+      );
+    },
+    onerror: function () {
+      console.error('Error loading Google API client library');
+    },
+  });
+}
 
-  const password = document.getElementById('password').value;
-
-  // TODO: Implement your login logic here
-});
-
-function toggleRequestStatus(button) {
-  const row = button.parentElement.parentElement;
-  if (row.classList.contains('incomplete')) {
-    row.classList.remove('incomplete');
-    row.classList.add('complete');
-    button.textContent = 'Completed';
+function updateSignInStatus(isSignedIn) {
+  if (isSignedIn) {
+    console.log('User is signed in');
   } else {
-    row.classList.remove('complete');
-    row.classList.add('incomplete');
-    button.textContent = 'Incomplete';
+    console.log('User is not signed in');
   }
 }
 
-// Add new maintenance request to table
-function addRequest(location, room, staff, date, description) {
-  const newRow = document.createElement('tr');
-  newRow.classList.add('incomplete');
-
-  newRow.innerHTML = `
-      <td>${location}</td>
-      <td>${room}</td>
-      <td>${staff}</td>
-      <td>${date}</td>
-      <td>${description}</td>
-      <td>
-        <button class="status-btn">Incomplete</button>
-      </td>
-  `;
-  
-  const requestsList = document.getElementById('requests-list');
-  requestsList.appendChild(newRow);
-}
+// Call the initClient function to load the Google API client and auth2 library
+initClient();
